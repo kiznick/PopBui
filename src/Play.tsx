f21a5026b57feb4b317a99840c17f7ff3a12102f
Yoswaris Lawpaiboon <me@kiznick.in.th>
2024-06-24 13:06:04 +0700

    Update Play.tsx

--- src/Play.tsx
+++ src/Play.tsx
@@ -22,7 +22,7 @@ function Play() {
 	const apiServer = 'https://popbui-api.kiznick.me/'
 	// const apiServer = 'http://localhost:3000/'
 
-	const maxClick = 25 * time
+	const maxClick = 30 * time
 
 	const medalEmoji: { [key: number]: string } = {
 		1: '🥇',
